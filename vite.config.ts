f1b05ae93beac852169840f2e4186529013e705d
Tomás Rodríguez <torodriguezt@unal.edu.co>
2024-12-29 21:40:37 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/PairedBayes-Docs/", // Reemplaza "your-repo-name" con el nombre de tu repositorio
+  base: "/PairedBayes-Docs/", 
 });
